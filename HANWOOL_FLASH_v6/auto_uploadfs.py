b7d930d9baece1db494386927e27f2df2fd0e6a3
HANWOOL <118036855+YBB-space@users.noreply.github.com>
2026-01-31 17:59:21 +0900

    Delete HANWOOL_FLASH_v6 directory

--- HANWOOL_FLASH_v6/auto_uploadfs.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-
-def before_upload(source, target, env):
-    print("=== auto uploadfs before normal upload ===")
-    # 먼저 LittleFS(또는 SPIFFS) 파일 시스템 업로드
-    env.Execute("pio run -t uploadfs")
-
-# 'upload' 타겟 실행 전에 위 함수를 한 번 호출하게 등록
-env.AddPreAction("upload", before_upload)
--- HANWOOL_FLASH_v6/src/config.cpp
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-const char* FW_PROGRAM     = "Intelligent FLASH V1.0";
-const char* FW_PROG_VER    = "v6";
-const char* FW_PROG_BUILD  = "B0.3";
-
-const char* FW_VER_BUILD   = "FLASH-F v1 · B0";
-const char* FW_BOARD       = "ALTIS INTELLIGENT V1";
-const char* FW_PROTOCOL    = "FLASH JSON v1.0";
